4e881dd199028b7547bd84bbe7c8a7ee16514592
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-30 21:15:27 -0300

    feito

---
+++ php/conexao.php
@@ -0,0 +1,17 @@
+<?php
+//informações para conectar no banco de dados
+
+$servidor = 'localhost';
+$usuario = 'root';
+$senha ='root';
+$bd= 'db_contabilidade';
+
+$conexao = new mysqli($servidor,$usuario,$senha,$bd);
+
+if ($conexao->connect_error){
+
+die('falha na connexao '.$conexao->connect_error);
+}else{
+
+    echo "conectado com sucesso";
+}
---
+++ php/insert.usuario.php
@@ -0,0 +1,13 @@
+<?php
+include 'conexao.php'; 
+
+//receber os dadsos do formulário
+$nomeCompleto=$_POST['nome'];
+$email=$_POST['email'];
+$cargo=$_POST['cargo'];
+$senha=$_POST['senha'];
+
+
+
+$sql="INSERT INTO tb_user values (null,'$nomeCompleto','$email','$cargo','$senha')";
+$conexao->query($sql);
