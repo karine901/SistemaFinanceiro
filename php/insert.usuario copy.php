f2159480d6566dd59c57c0e4ffe1ff72906e2c04
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-10-21 21:48:29 -0300

    feito

---
+++ php/insert.usuario copy.php
@@ -0,0 +1,18 @@
+<?php
+include 'conexao.php'; 
+
+//receber os dados do formulário
+$nomeCompleto=$_POST['nome'];
+$tipo=$_POST['tipo'];
+$descricao=$_POST['descricao'];
+
+
+
+$sql="INSERT INTO tb_categoria values (null,'$nomeCompleto','$tipo','$descricao')";
+
+if ($conexao->query($sql)) {
+    echo "<script>alert('Inserido com sucesso!'); history.back();</script>";
+}else {
+    echo "Falha na conexão com banco de dados";
+}
+ 
